96ab78abe6f45fede0c016baf768976bace54258
KevenChampagne <kevenchampagne@hotmail.com>
2023-02-01 15:24:27 -0500

    test

--- ProjetSunthese/Assets/Script/EnemyController.cs
+++ ProjetSunthese/Assets/Script/EnemyController.cs
@@ -5,9 +5,8 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
     }
 
     // Update is called once per frame
