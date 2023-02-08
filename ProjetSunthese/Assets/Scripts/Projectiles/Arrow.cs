57e4ada3c65b66666a6c4d0e7eeaa937a182679b
Marc-Antoine Girard-Gagnon <ggmarcantoine474@gmail.com>
2023-02-08 11:33:46 -0500

    archer mostly done, ready to implement

---
+++ ProjetSunthese/Assets/Scripts/Projectiles/Arrow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : Projectile
+{
+    private Sensor sensor;
+    private ISensor<Enemy> enemySensor;
+    private void Awake()
+    {
+        sensor = GetComponentInChildren<Sensor>();
+        enemySensor = sensor.For<Enemy>();
+        enemySensor.OnSensedObject += OnEnemySense;
+        enemySensor.OnUnsensedObject += OnEnemyUnsense;
+    }
+
+    private void OnEnemySense(Enemy enemy)
+    {
+        enemy.Harm(damage);
+        gameObject.SetActive(false);
+    }
+
+    private void OnEnemyUnsense(Enemy enemy)
+    {
+
+    }
+
+    /*protected override void DestinationReached()
+    {
+    }*/
+}
