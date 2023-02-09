c007cfd74bea5e902b31576b16b8eb0f29af4166
Gabriel Bertrand <gabriel_bertrand@hotmail.com>
2023-02-09 11:31:12 -0500

    ajout des sensors avec attaque

--- ProjetSunthese/Assets/Scripts/Boss 1/LaserPoint.cs
+++ ProjetSunthese/Assets/Scripts/Boss 1/LaserPoint.cs
@@ -6,9 +6,17 @@ public class LaserPoint : MonoBehaviour
 {
     private Animator animator;
 
-    private void Start()
+    [SerializeField] private Sensor sensor;
+    private ISensor<Player> playerSensor;
+
+    private void Awake()
     {
         animator = GetComponentInChildren<Animator>();
+
+        playerSensor = sensor.For<Player>();
+        playerSensor.OnSensedObject += OnPlayerSense;
+        playerSensor.OnUnsensedObject += OnPlayerUnsense;
+        DeactivateSensor();
     }
 
     public void StartMovement(Vector3 pivotPoint)
@@ -18,10 +26,30 @@ public class LaserPoint : MonoBehaviour
 
     private IEnumerator Rotate(Vector3 pivotPoint)
     {
+        ActivateSensor();
         while (true)
         {
             transform.RotateAround(pivotPoint, new Vector3(0, 0, 1), Time.deltaTime * 50);
             yield return null;
         }
+    }
+
+    private void OnPlayerSense(Player otherObject)
+    {
+        Debug.Log("in");
+    }
+
+    private void OnPlayerUnsense(Player otherObject)
+    {
+        Debug.Log("out");
+    }
+
+    void ActivateSensor()
+    {
+        sensor.gameObject.SetActive(true);
+    }
+    void DeactivateSensor()
+    {
+        sensor.gameObject.SetActive(false);
     }
 }
---
+++ ProjetSunthese/Assets/Scripts/Boss 1/LavaThrowAttack.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaThrowAttack : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab;
+
+    [Header("Base Stats")]
+    [SerializeField] private int numberLavaObjects;
+    [SerializeField] private int minToThrow;
+    [SerializeField] private int maxToThrow;
+    [SerializeField] private float minSpeed;
+    [SerializeField] private float maxSpeed;
+
+    [Header("Distance")]
+    [SerializeField] private Transform minDistance;
+    [SerializeField] private Transform maxDistance;
+
+    private GameObject[] lavaObjects;
+
+    void Start()
+    {
+        lavaObjects = new GameObject[numberLavaObjects];
+
+        for (int i = 0; i < numberLavaObjects; i++)
+        {
+            lavaObjects[i] = Instantiate(prefab, transform);
+            lavaObjects[i].SetActive(false);
+        }
+    }
+
+    public void Launch()
+    {
+        // max is exclusive
+        int rand = Random.Range(minToThrow, maxToThrow + 1);
+        int count = 0;
+
+        foreach(GameObject lava in lavaObjects)
+        {
+            if(!lava.activeSelf)
+            {
+                lava.SetActive(true);
+                lava.gameObject.transform.position = transform.position;
+                lava.gameObject.GetComponent<LavaPoint>().Launch(
+                    new Vector2(
+                    Random.Range(minDistance.position.x, maxDistance.position.x),
+                    Random.Range(minDistance.position.y, maxDistance.position.y)));
+                count++;
+                if (count >= rand) break;
+            }
+        }
+    }
+}
