4deba7d6a3b3d490fc9e8c5254a7168c062b8a3b
Marc-Antoine Girard-Gagnon <ggmarcantoine474@gmail.com>
2023-02-21 12:24:35 -0500

    flips properly

--- ProjetSunthese/Assets/Scripts/Weapon/DaggersWeapon.cs
+++ ProjetSunthese/Assets/Scripts/Weapon/DaggersWeapon.cs
@@ -12,23 +12,36 @@ public class Daggers : Weapon
     [SerializeField] private float recoilDistance;
     [SerializeField] private Dagger dagger1;
     [SerializeField] private Dagger dagger2;
+    private Vector3 reversed = new Vector3(1, -1, 1);
 
     protected override void Start()
     {
         base.Start();
         dagger1.transform.parent.position = rotationPoint.position;
         dagger2.transform.parent.position = rotationPoint.position;
-        orbit = false;  
+        orbit = false;
     }
 
     protected override IEnumerator Attack()
     {
         cooldownTimer = cooldown + duration + startup + recovery + delay;
-        dagger1.Slash();
+        dagger1.StopOrbit();
         dagger2.StopOrbit();
+        dagger1.Slash();
         yield return new WaitForSeconds(delay);
         dagger2.Slash();
         yield return new WaitForSeconds(recovery);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (dagger1.GetShouldFlip())
+        {
+            transform.localScale = reversed;
+        }
+        else
+            transform.localScale = Vector3.one;
     }
 
     public override void SetPlayerBaseWeaponStat(PlayerBaseWeaponStat playerBaseWeaponStat)
