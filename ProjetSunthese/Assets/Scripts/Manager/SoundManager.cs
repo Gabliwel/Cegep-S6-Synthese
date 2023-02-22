f29d73896a535f448fe778816d10b0c975a29c58
KevenChampagne <kevenchampagne@hotmail.com>
2023-02-22 14:24:56 -0500

    Push to pull

---
+++ ProjetSunthese/Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance = null;
+    public static SoundManager Instance { get { return instance; } }
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
