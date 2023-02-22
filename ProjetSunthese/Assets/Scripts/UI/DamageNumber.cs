e8f40c430335fa4eddec2a3d23be16eb0e349bc8
Marc-Antoine Girard-Gagnon <ggmarcantoine474@gmail.com>
2023-02-22 12:33:16 -0500

    added fadeout

--- ProjetSunthese/Assets/Scripts/UI/DamageNumber.cs
+++ ProjetSunthese/Assets/Scripts/UI/DamageNumber.cs
@@ -6,8 +6,11 @@ using TMPro;
 public class DamageNumber : MonoBehaviour
 {
     [SerializeField] private float ttl;
+    [SerializeField] private float fadeoutAmmount;
     private float ttlTimer;
     private TextMeshProUGUI tmp;
+    private float fadeTimer;
+    private bool fading;
 
     private void Awake()
     {
@@ -16,13 +19,25 @@ public class DamageNumber : MonoBehaviour
     private void OnEnable()
     {
         ttlTimer = ttl;
+        fading = false;
     }
     private void Update()
     {
         if (ttlTimer > 0)
             ttlTimer -= Time.deltaTime;
-        else
-            gameObject.SetActive(false);
+        else if (!fading)
+        {
+            fading = true;
+        }
+        if (fading)
+        {
+            fadeTimer -= Time.deltaTime;
+            Color color = tmp.color;
+            color.a -= Time.deltaTime * fadeoutAmmount;
+            tmp.color = color;
+            if (color.a <= 0)
+                gameObject.SetActive(false);
+        }
     }
 
     public void SetText(string text, Color color)
